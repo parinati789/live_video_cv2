f2696b29b1bddd4dc9c012c46a0ebb32e5a0353a
parinati789 <75272907+parinati789@users.noreply.github.com>
2021-06-10 00:54:26 +0530

    new
    new

---
+++ server.py
@@ -0,0 +1,33 @@
+import cv2, imutils, socket
+import numpy as np
+import base64
+
+BUFF_SIZE = 65536
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+host_name = socket.gethostname()
+host_ip = '192.168.1.102'#  socket.gethostbyname(host_name)
+print(host_ip)
+port = 1234
+socket_address = (host_ip,port)
+server_socket.bind(socket_address)
+print('Listening at:',socket_address)
+
+vid = cv2.VideoCapture(0) #  replace 'rocket.mp4' with 0 for webcam
+fps,st,frames_to_count,cnt = (0,0,20,0)
+
+while True:
+	msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
+	print('GOT connection from ',client_addr)
+	WIDTH=400
+	while(vid.isOpened()):
+		_,frame = vid.read()
+		frame = imutils.resize(frame,width=WIDTH)
+		encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+		message = base64.b64encode(buffer)
+		server_socket.sendto(message,client_addr)
+		cv2.imshow('TRANSMITTING VIDEO',frame)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord('q'):
+			server_socket.close()
+			break
